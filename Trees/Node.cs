a6ae9eac3b431eae413f5f314c8965fb64c0076f
Billy Soomro <billy.soomro@worldpay.com>
2022-03-10 18:45:52 +0000

    Add project files.

---
+++ Trees/Node.cs
@@ -0,0 +1,11 @@
+﻿namespace Trees
+{
+    public class Node
+    {
+        public int Data { get; set; }
+
+        public Node LeftNode { get; set; }
+
+        public Node RightNode { get; set; }
+    }
+}
---
+++ Trees/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rootNode = new Node
+            {
+                Data = 25,
+                LeftNode = new Node
+                {
+                    Data = 10,
+                    LeftNode = new Node
+                    {
+                        Data = 15,
+                    },
+                    RightNode = new Node
+                    {
+                        Data = 10
+                    }
+                },
+                RightNode = new Node
+                {
+                    Data = 15,
+                    LeftNode = new Node
+                    {
+                        Data = 15,
+                    },
+                    RightNode = new Node
+                    {
+                        Data = 10
+                    }
+                }
+            };
+            
+            Console.WriteLine($"Tree sum: {GetTreeSum(rootNode)}");
+            Console.ReadLine();
+        }
+
+        private static int GetTreeSum(Node rootNode)
+        {
+            if (rootNode == null) return 0;
+
+            return rootNode.Data + GetTreeSum(rootNode.LeftNode) + GetTreeSum(rootNode.RightNode);
+        }
+    }
+}
